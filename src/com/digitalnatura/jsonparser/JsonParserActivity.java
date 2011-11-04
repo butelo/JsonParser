14bba2f694df07ed8c8227c8efc1bedb6c50da05
xes <xes@xes-901>
2011-11-04 10:59:33 +0100

    f

---
+++ src/com/digitalnatura/jsonparser/JsonParserActivity.java
@@ -0,0 +1,128 @@
+package com.digitalnatura.jsonparser;
+
+//import com.digitalnatura.filemanager.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.api.client.json.JsonParser;
+import com.google.api.client.json.JsonToken;
+import com.google.api.client.json.jackson.JacksonFactory;
+
+public class JsonParserActivity extends Activity {
+    private static final String LOGTAG = null;
+	private String TAG = "logaritmo";
+	private InputStream celdas;
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        AssetManager am = getAssets();
+//        String myString = IOUtils.toString(myInputStream, "UTF-8");
+//        am.
+
+       
+			 try {
+				celdas =am.open("celdas.json");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			 
+			 	try {
+					parserarJson();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+		  
+			
+			
+		     
+		     
+		     
+		     
+		 
+		
+		
+    }
+    
+    public String convertStreamToString(InputStream is)
+    throws IOException {
+/*
+ * To convert the InputStream to String we use the
+ * Reader.read(char[] buffer) method. We iterate until the
+ * Reader return -1 which means there's no more data to
+ * read. We use the StringWriter class to produce the string.
+ */
+if (is != null) {
+    Writer writer = new StringWriter();
+
+    char[] buffer = new char[1024];
+    try {
+        Reader reader = new BufferedReader(
+                new InputStreamReader(is, "UTF-8"));
+        int n;
+        while ((n = reader.read(buffer)) != -1) {
+            writer.write(buffer, 0, n);
+        }
+    } finally {
+        is.close();
+    }
+    return writer.toString();
+} else {        
+    return "";
+}
+}
+
+	private void parserarJson() throws IOException {
+		// TODO Auto-generated method stub
+		JacksonFactory f = new JacksonFactory();     
+	     
+		JsonParser jParser = f.createJsonParser(convertStreamToString(celdas));
+		
+		 while (jParser.nextToken() != JsonToken.END_OBJECT) {
+			 while (jParser.nextToken() != JsonToken.END_OBJECT) {
+				 String fieldname = jParser.getCurrentName();
+				 Log.e(JsonParserActivity.LOGTAG,"algo "+ fieldname);
+
+				 while (jParser.nextToken() != JsonToken.END_OBJECT) {
+
+	        	 if ("openSearch$totalResults".equals(fieldname)){
+//	        		 while (jParser.nextToken() != JsonToken.END_OBJECT) {
+            	            	 String id1 = jParser.getText();
+
+	        		 }   					 
+				 }   				
+			 }   		
+	 }
+	 
+	 jParser.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		Log.e(TAG   , celdas);
+	}
+    
+}
